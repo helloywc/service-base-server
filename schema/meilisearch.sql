f0b672b194c92991b95426d5e60c319b0758356f
Wilson 1 <>
2026-03-24 18:16:51 +0800

    perf: 加入keywords字段

--- schema/meilisearch.sql
+++ schema/meilisearch.sql
@@ -2,6 +2,7 @@ index medias (
     id INT PRIMARY KEY,
     key VARCHAR(255),
     title VARCHAR(255),
+    keywords VARCHAR(255),
     content TEXT,
     summary VARCHAR(255),
     remark VARCHAR(255),
